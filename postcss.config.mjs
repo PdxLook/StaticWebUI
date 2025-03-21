c02a44c89332a69a48cfe9bfd76477ae9a489724
Mjx <798751550@qq.com>
2025-03-21 16:53:09 +0800

    cli: scss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,14 +1,30 @@
 import UnoCSS from "@unocss/postcss";
 import cssnano from "cssnano";
-import postcssImport from "postcss-import"; // 使用 import 替代 require
+import postcssImport from "postcss-import";
+import postcssPresetEnv from "postcss-preset-env";
 
 export default {
   syntax: "postcss-scss",
+  parser: "postcss-scss",
   plugins: [
     postcssImport({
-      path: ["src/scss"],
+      path: ["static_assets/scss"],
+    }),
+    postcssPresetEnv({
+      features: {
+        "nesting-rules": true,
+        "custom-properties": true,
+        "custom-media-queries": true,
+        "media-query-ranges": true,
+        "custom-selectors": true,
+        "gap-properties": true,
+        "not-pseudo-class": true,
+        "focus-visible-pseudo-class": true,
+        "focus-within-pseudo-class": true,
+        "color-functional-notation": true,
+      },
     }),
     UnoCSS(),
-    cssnano(), // 压缩 CSS
+    cssnano(),
   ],
 };
